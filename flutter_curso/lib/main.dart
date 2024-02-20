2031f9c24277e55360907fe5e3e396b1c54d11b0
JarlissonTomaz <tomazj995@gmail.com>
2024-02-20 14:08:05 -0300

    class 3

--- flutter_curso/lib/main.dart
+++ flutter_curso/lib/main.dart
@@ -9,12 +9,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('My First App'),
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('Meu Primeiro App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('A pergunta!'),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0),
+                  child: ElevatedButton(
+                    child: const Text('Resposta 1'),
+                    onPressed: () => print('Resposta 1'),
+                  ),
+                ),
+                ElevatedButton(
+                  child: const Text('Resposta 2'),
+                  onPressed: () => print('Resposta 2'),
+                ),
+                ElevatedButton(
+                  child: const Text('Resposta 3'),
+                  onPressed: () => print('Resposta 3'),
+                ),
+              ],
+            ),
+          ],
         ),
-        body: const Center(
-          child: Text("Hello World!!"),
-        ));
+      ),
+    ));
   }
 }
